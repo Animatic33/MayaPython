942ea6898e920a3be64786826b76aec6e33690ff
Animatic33 <ag.animator@gmail.com>
2016-02-07 15:06:08 -0500

    moved readme data from agMayaQuickSelect.py to README.md. Added installguide and small icon for usage

--- agMayaQuickSelect.py
+++ agMayaQuickSelect.py
@@ -9,49 +9,8 @@ First Release: Feb 5 2016
 Updates:
 February 5 2016 - Initial Release (0.1)
 
--------------------
-Q: What does this do?
-    Quick History Selection opens up a window that can temporarily hold selection sets 
-    (not the built-in Maya sets) that you can easily toggle between.
-
-Q: Why not just use regular quick selection sets?
-    Primarily because sometimes you just want to save a selection 
-    of something (ie. vertices) for a short period of time.
-    Saving out as sets seems a little unnecessary, and this is 
-    just a quick and dirty approach with some key features that
-    relay a little more information at glance than selection sets.
-    
-Q: What versions of Maya support this?
-    Hopefully all of them? I developed this and tested it 
-    in Maya 2011 (yeah, it's an old copy!) I don't provide any sort of
-    assistance or guarantees with this script beyond claiming it will 
-    work. Don't use it in a commercial environment until it's fully
-    tested to do what you expect. No modifications to the scene are 
-    made unless it's the 'globalSelectionDictionary' dictionary object
-    that we instantiate before running the script
--------------------
-
-Usage: 
-- Include agMayaQuickSelect.py in scripts folder
-- in maya's python script editor run:
-
-    # this can be named anything you want, just make sure to pass it in to the selectHistoryUI next
-    globalSelectionDictionary = {}  
-   
-    # if you want to make a shelf button, just drag the following lines to the shelf
-    import agMayaQuickSelect as qsh
-    reload(qsh) # just in case
-    qsh.selectHistoryUI(globalSelectionDictionary)
-   
-------------------
-Further Notes:
-
-- Since 'globalSelectionDictionary' is a dictionary being instantiated in Maya's local space, it should stick around between closing/opening
-  the UI window. Since this is not saved in any way after the program exits, all that data will be lost. Maybe in the future I'll
-  add an option for JSON or something.
-  
-- Don't re-instantiate the dictionary unless you wish for all your selection data to be deleted! 
-
+Readme: Readme.md
+Install Guide: InstallGuide.txt
 """
 import maya.cmds as cmds
 
